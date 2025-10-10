af595734528c0d65759e868560deafff78ebe46c
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-10-10 23:55:01 +0200

    Added ollama test

--- server/src/server.js
+++ server/src/server.js
@@ -3,27 +3,38 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-    res.status(200.).send('Hello world');
-})
+  res.status(200).send('Hello world');
+});
 
 app.get("/api/chat", async (req, res) => {
   try {
-    const response = await fetch("http://ollama:11434/api/generate", {
+
+    const prompt = req.body.prompt
+
+    const response = await fetch("http://ollama:11434/api/generate", { 
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        model: "llama3",
-        prompt: "Hello from Express!",
+        model: "mistral",
+        prompt: prompt,
+        stream: false
       }),
     });
 
     const data = await response.json();
-    res.json(data);
+    const output = data.response || "No response generated";
+    
+    res.send(output);
+
   } catch (err) {
     console.error("Ollama error:", err);
     res.status(500).json({ error: "Failed to connect to Ollama" });
   }
 });
+
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
