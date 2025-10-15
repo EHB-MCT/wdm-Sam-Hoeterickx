2924b871e4d164e560a2f55d90186753b44745d5
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-10-15 15:37:03 +0200

    Added base for answerQuestion

--- server/src/answers/controller.js
+++ server/src/answers/controller.js
@@ -1,5 +1,15 @@
 const answerQuestion = (req, res, collection) => {
+    try{
 
+        
+
+    }catch(error){
+        console.error('Create answer error:', error);
+        return res.status(500).send({
+            status: 500,
+            message: error.message
+        })
+    }
 }
 
 module.exports = {
