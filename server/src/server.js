de2c37acaaa7a5dc75d98f5e0908dc5b4747cdc9
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-10-15 10:14:42 +0200

    Init mongodb connection

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,21 @@
+require('dotenv').config();
+
 const express = require('express');
+const { MongoClient } = require('mongodb');
 
 const app = express();
 const port = 3000;
+
+const DB_URI = process.env.DB_URI;
+const CLIENT = new MongoClient(DB_URI);
+const DB_NAME = 'Development-V-WDM';
+const DATABASE = CLIENT.db(DB_NAME);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 //Collections
-const questionsCollection = ['hello', 'there', 'hellowww','world'];
+const questionsCollection = DATABASE.collection('questions')
 
 //Routes
 const ollamaRouter = require('./ollama/route.js');
