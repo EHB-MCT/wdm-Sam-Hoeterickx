d323f3c5d98ad293ace64ae3109aa4884f6309c2
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-10-15 15:54:33 +0200

    WIP handle answer question

--- client/src/modules/app/App.jsx
+++ client/src/modules/app/App.jsx
@@ -8,6 +8,14 @@ import { useQuestions } from "../../shared/hooks/useQuestions.hook";
 
 export const App = () => {
 
+  const [answer, setAnswer] = useState({
+    question_id: undefined,
+    answer: undefined,
+    descion_time: undefined,
+    elapsedHoverTime: undefined,
+    changedMind: undefined 
+  })
+
   const { question, nextQuestion, getDecisionTime } = useQuestions();
   const { elapsedHoverTime, handleMouseEnter, handleMouseLeave, resetHoverTime } = useHoverTracking();
   const { changedMind, updateChoice, resetChoices } = useChoiceTracking();
@@ -30,18 +38,47 @@ export const App = () => {
   const handleButtonClick = (id) => {
     handleMouseLeave(id);
     updateChoice(id);
+    setAnswer(prev => ({
+      ...prev,
+      answer: id
+    }));
   }
 
   const handleNextButton = () => {
-    console.log("Decision time:", getDecisionTime(), "s");
+    const decisionTime = getDecisionTime()
+    console.log("Decision time:", decisionTime, "s");
     console.log("Hover times:", elapsedHoverTime);
     console.log("Changed mind:", changedMind);
 
-    resetHoverTime();
-    resetChoices();
 
-    nextQuestion();
 
+    handleAnswerQuestion();
+
+    setAnswer(prev => ({
+      ...prev,
+      question_id: undefined,
+      descion_time: decisionTime,
+      elapsedHoverTime: elapsedHoverTime,
+      changedMind: changedMind 
+    }))
+
+  }
+
+  const handleAnswerQuestion = () => {
+    fetch('http://localhost:3000/api/answers/saveAnswer', {
+      method: 'POST',
+      credentials: 'include',
+      body: JSON.stringify({
+
+      })
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .then(() => {
+      // resetHoverTime();
+      // resetChoices();
+      //nextQuestion();
+    })
   }
 
   return (
--- server/src/answers/controller.js
+++ server/src/answers/controller.js
@@ -1,6 +1,13 @@
 const answerQuestion = (req, res, collection) => {
     try{
-        
+
+        const SESSION_ID = req.signedCookies.session;
+        console.log("session id", SESSION_ID);
+
+        return res.status(201).send({
+            status: 201,
+            message: 'Answer saved successfully'
+        });
 
     }catch(error){
         console.error('Create answer error:', error);
