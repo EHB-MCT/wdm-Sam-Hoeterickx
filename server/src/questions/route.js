3df1f1bec55d2eedf8f202d56655eb15387bfc06
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-12-22 15:15:08 +0100

    Feat(api/questions): Trying to fix doctype error, updated route to get questions

--- server/src/questions/route.js
+++ server/src/questions/route.js
@@ -5,7 +5,7 @@ const { getQuestions, addQuestions } = require('./controller.js');
 
 module.exports = (collection) => {
     
-    router.get('/questions', (req, res) => getQuestions(req, res, collection));
+    router.get('/', (req, res) => getQuestions(req, res, collection));
     router.post('/questions', (req, res) => addQuestions(req, res, collection));
     return router
     
