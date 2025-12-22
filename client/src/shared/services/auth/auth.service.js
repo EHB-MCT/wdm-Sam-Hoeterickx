3df0d2c5a7f02ad0fa76df9f4dbac56ae79a13f2
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-12-22 14:27:18 +0100

    Feat(auth/service):  Updated body for register and login

--- client/src/shared/services/auth/auth.service.js
+++ client/src/shared/services/auth/auth.service.js
@@ -1,70 +1,56 @@
-const BASE_URL = `${import.meta.env.VITE_API_URL}/auth`;
-
 class AuthService {
     async registerUser(username, email, password, repeatPassword){
         const response = await fetch(`${BASE_URL}/register`, {
             method: 'POST',
-            headers: {
-                'Content-Type': "application/json"
-            },
-            body: {
-                username,
-                email,
-                password,
-                repeatPassword
-            }
+            headers: { 'Content-Type': "application/json" },
+            body: JSON.stringify({ 
+                username, 
+                email, 
+                password, 
+                repeatPassword 
+            })
         });
 
         if(!response.ok){
             const errorData = await response.json();
-            console.log(errorData)
-            throw new Error(`${errorData.status} | ${errorData.message}` || 'Failed to register');
+            throw new Error(errorData.message || 'Failed to register');
         }
 
-        const data = await response.json();
-        return data;
+        return await response.json();
     }
 
     async loginUser(email, password){
         const response = await fetch(`${BASE_URL}/login`, {
             method: 'POST',
-            headers: {
-                'Content-Type': "application/json"
-            },
-            credentials: 'include',
-            body: {
-                email,
-                password,
-            }
+            headers: { 'Content-Type': "application/json" },
+            credentials: 'include', 
+            body: JSON.stringify({ 
+                email, 
+                password, 
+            })
         });
 
         if(!response.ok){
             const errorData = await response.json();
-            console.log(errorData)
-            throw new Error(`${errorData.status} | ${errorData.message}` || 'Failed to login');
+            throw new Error(errorData.message || 'Failed to login');
         }
 
-        const data = await response.json();
-        return data;
+        return await response.json();
     }
 
     async logoutUser(){
-        const response = await fetch(`${BASE_URL}/lgout`, {
+        const response = await fetch(`${BASE_URL}/logout`, { 
             method: 'POST',
             credentials: 'include',
-            headers: {
-                'Content-Type': "application/json"
-            },
+            headers: { 'Content-Type': "application/json" },
         });
 
         if(!response.ok){
             const errorData = await response.json();
-            console.log(errorData)
-            throw new Error(`${errorData.status} | ${errorData.message}` || 'Failed to logout');
+            throw new Error(errorData.message || 'Failed to logout');
         }
 
-        const data = await response.json();
-        return data;
+        return await response.json();
     }
 }
 
