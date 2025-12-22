80f8665b99ceebec31c5d7ab5dcc5665945281df
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-12-22 14:40:31 +0100

    Feat(auth/hook): Added logout hook

---
+++ client/src/shared/hooks/auth/useLogoutUser.hoook.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+
+//Service
+import { authService } from "../../services";
+
+export const useRegisterUser = () => {
+
+    const [isLoading, setIsLoading] = useState(false);
+    const [isSuccess, setIsSuccess] = useState(undefined);
+    const [error, setError] = useState({
+        status: undefined,
+        message: undefined
+    });
+
+    const logout = async (onSuccess) => {
+        setIsLoading(true);
+        setIsSuccess(undefined);
+        setError({ 
+            status: undefined, 
+            message: undefined 
+        });
+
+        try {
+            await authService.logoutUser();
+
+            setIsSuccess(true);
+            if(onSuccess) onSuccess();
+
+        }catch (error) {
+            setIsSuccess(false);
+            setError({ 
+                status: error.status,
+                message: error.message 
+            });
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    return { logout, isLoading, isSuccess, error };
+};
