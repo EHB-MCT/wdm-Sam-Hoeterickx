d27f71bbdcbd10adf91c2acee79f0c37377c16c2
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-12-22 13:56:02 +0100

    Feat(api/auth): Added password requirements

--- server/src/users/controller.js
+++ server/src/users/controller.js
@@ -24,6 +24,34 @@ const loginUser = async(req, res, collection) => {
             });
         };
 
+        if (password.length < 8) {
+            return res.status(422).send({
+                status: 422,
+                message: "Password must be at least 8 characters long"
+            });
+        }
+
+        if (!/[A-Z]/.test(password)) {
+            return res.status(422).send({
+                status: 422,
+                message: "Password must contain at least one uppercase letter"
+            });
+        }
+
+        if (!/[a-z]/.test(password)) {
+            return res.status(422).send({
+                status: 422,
+                message: "Password must contain at least one lowercase letter"
+            });
+        }
+
+        if (!/[0-9]/.test(password)) {
+            return res.status(422).send({
+                status: 422,
+                message: "Password must contain at least one number"
+            });
+        }
+
         const user = await findUserByEmail(collection, email);
         
         if(!user){
