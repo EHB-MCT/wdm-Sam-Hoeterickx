7cefaec91739fbe049c64e192a17cf0a873c986c
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-12-22 14:17:23 +0100

    Feat(auth/service): Added login function

--- client/src/shared/services/auth/auth.service.js
+++ client/src/shared/services/auth/auth.service.js
@@ -14,11 +14,33 @@ class AuthService {
                 repeatPassword
             }
         });
-        
+
         if(!response.ok){
             const errorData = await response.json();
             console.log(errorData)
             throw new Error(`${errorData.status} | ${errorData.message}` || 'Failed to register');
+        }
+
+        const data = await response.json();
+        return data;
+    }
+
+    async loginUser(email, password){
+        const response = await fetch(`${BASE_URL}/login`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': "application/json"
+            },
+            body: {
+                email,
+                password,
+            }
+        });
+
+        if(!response.ok){
+            const errorData = await response.json();
+            console.log(errorData)
+            throw new Error(`${errorData.status} | ${errorData.message}` || 'Failed to login');
         }
 
         const data = await response.json();
