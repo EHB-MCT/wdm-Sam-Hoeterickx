6999d0238d6ca5aad41fe6e444579f308ac784e5
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-12-22 17:20:37 +0100

    Feat(dashboard): Added service for users

---
+++ client/src/shared/hooks/user/user.service.js
@@ -0,0 +1,17 @@
+const BASE_URL = `${import.meta.env.VITE_API_URL}/user`;
+
+class UserService {
+    async getUserData(){
+        const response = await fetch(`${BASE_URL}`, {
+            method: 'GET',
+            credentials: 'include'
+        });
+
+        if(!response.ok){
+            const errorData = await response.json();
+            throw new Error(errorData.message || 'Failed to register');
+        }
+
+        return await response.json();
+    }
+}
