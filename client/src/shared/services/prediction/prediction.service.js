f3e118b90f94f92704c71c0939f019ed0eb3ad93
Sam-Hoeterickx <sam.hoeterickx@student.ehb.be>
2025-12-27 09:56:33 +0100

    Feat(prediction): Added documentation to prediction service

--- client/src/shared/services/prediction/prediction.service.js
+++ client/src/shared/services/prediction/prediction.service.js
@@ -1,6 +1,21 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
 
+/**
+ * Service class responsible for handling prediction-related API interactions.
+ * Collects and sends user behavioral data to the backend for analysis.
+*/
+
 class PredictionService {
+
+    /**
+     * Sends user behavior metrics to the server to save a prediction state.
+     * 
+     * @param {boolean} changedMindState - Indicates whether the user changed their mind/selection before submitting.
+     * @param {number} elapsedHoverTime - The total duration (in milliseconds) the user hovered over the options.
+     * @param {number} desicionTime - The time (in milliseconds) it took the user to make a decision.
+     * @returns {Promise<Object>} A promise that resolves to the server response data.
+     * @throws {Error} If the API request fails or returns a non-OK status.
+    */
 
     async savePredictionData(changedMindState, elapsedHoverTime, desicionTime){
         const response = await fetch(`${BASE_URL}/prediction/savePrediction`, {
