4e2c1f126a6dc44212b0aa46bd2db645d747b921
Sam-Hoeterickx <samhoeterickx111@gmail.com>
2025-12-28 20:37:52 +0100

    Feat(geolocation): Added check if there is locatoinData

--- client/src/shared/services/geoLocation/geoLocation.service.js
+++ client/src/shared/services/geoLocation/geoLocation.service.js
@@ -6,14 +6,23 @@ const BASE_URL = `${import.meta.env.VITE_API_URL}/geolocation`;
 
 class GeoLocationService {
 
+
+
     /**
-     * Registers a new user.
+     * Saves geolocation data when available.
      * 
      * @param {Object} locationData - The desired username
      * @returns {Promise<Object>} The server response data upon success
      * @throws {Error} Throws an error if saving fails
      */
     async saveGeoLocation(locationData, ){
+
+        if (!locationData) {
+            const error = new Error("Location data is required");
+            setSaveError(error.message);
+            throw error;
+        }
+
         const response = await fetch(`${BASE_URL}/save`, {
             method: 'POST',
             credentials: 'include',
